4b2af3c026883068c5640dcf6d38fb00709bbee1
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-05-12 09:21:57 -0600

    ORM QUERY

--- Python/orm.py
+++ Python/orm.py
@@ -1,41 +1,117 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, text
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
 import time
+from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey, create_engine
+from sqlalchemy.types import LargeBinary
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from geoalchemy2 import Geography
 
 # create engine and session
 engine = create_engine("mssql+pyodbc://localhost\SQLEXPRESS/caso3?driver=ODBC+Driver+17+for+SQL+Server")
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# create model for the producers table
+# create base class
 Base = declarative_base()
+
+# define models for the result set
+class WasteMovement(Base):
+    __tablename__ = 'wasteMovements'
+    wasteMovementId = Column(Integer, primary_key=True)
+    posttime = Column(Date, nullable=False)
+    responsibleName = Column(String(50), nullable=False)
+    signImage = Column(LargeBinary, nullable=False)
+    addressId = Column(Integer, ForeignKey('addresses.addressId'), nullable=False)
+    movementTypeId = Column(Integer, nullable=False)
+    contractId = Column(Integer, nullable=False)
+    quantity = Column(Float(precision=2), nullable=False)
+    userId = Column(Integer, nullable=False)
+    checksum = Column(LargeBinary, nullable=False)
+    computer = Column(String(50), nullable=False)
+    containerId = Column(Integer, ForeignKey('containers.containerId'), nullable=False)
+    wasteId = Column(Integer, ForeignKey('wastes.wasteId'), nullable=False)
+    carId = Column(Integer)
+    address = relationship("Address")
+    container = relationship("Container")
+    waste = relationship("Waste")
+
+class Waste(Base):
+    __tablename__ = 'wastes'
+    wasteId = Column(Integer, primary_key=True)
+    wasteType = Column(Integer, nullable=False)
+    wasteName = Column(String(50), nullable=False)
+
+class WasteType(Base):
+    __tablename__ = 'wasteTypes'
+    wasteTypeId = Column(Integer, primary_key=True)
+    typeName = Column(String(50), nullable=False)
+    recyclable = Column(LargeBinary, nullable=False)
+
+class Address(Base):
+    __tablename__ = 'addresses'
+    addressId = Column(Integer, primary_key=True)
+    countryId = Column(Integer, ForeignKey('countries.countryId'), nullable=False)
+    stateId = Column(Integer, nullable=False)
+    cityId = Column(Integer, nullable=False)
+    geoLocation = Column(Geography, nullable=False)
+    country = relationship("Country")
+
+class Country(Base):
+    __tablename__ = 'countries'
+    countryId = Column(Integer, primary_key=True)
+    countryName = Column(String(50), nullable=False)
+
+class Container(Base):
+    __tablename__ = 'containers'
+    containerId = Column(Integer, primary_key=True)
+    containerName = Column(String(50), nullable=False)
+    description = Column(String(50), nullable=False)
+    containerTypeId = Column(Integer, ForeignKey('containerTypes.containerTypeId'), nullable=False)
+    containerType = relationship("ContainerType")
+
+class ContainerType(Base):
+    __tablename__ = 'containerTypes'
+    containerTypeId = Column(Integer, primary_key=True)
+    typeName = Column(String(50), nullable=False)
+    brandId = Column(Integer, nullable=False)
+    modelId = Column(Integer, nullable=False)
+    capacity = Column(Float(precision=2), nullable=False)
+    measureId = Column(Integer, nullable=False)
+
+class ProducerXMovement(Base):
+    __tablename__ = 'producersXmovements'
+    producerXmovement = Column(Integer, primary_key=True)
+    producerId = Column(Integer, ForeignKey('producers.producerId'), nullable=False)
+    wasteMovementId = Column(Integer, ForeignKey('wasteMovements.wasteMovementId'), nullable=False)
 
 class Producer(Base):
     __tablename__ = 'producers'
     producerId = Column(Integer, primary_key=True)
     producerName = Column(String(50), nullable=False)
-    grade = Column(Float(5, 2))
-    balance = Column(Float(18, 2), nullable=False)
+    grade = Column(Float(precision=2))
+    balance = Column(Float(precision=2), nullable=False)
+# define the function to execute the query with the ORM
+def get_filtered_producers_orm(quantity):
+    results = session.query(WasteMovement.posttime, WasteMovement.quantity, Container.containerName,
+                            Waste.wasteName, WasteType.typeName, Producer.producerName,
+                            Country.countryName).\
+        join(Waste, Waste.wasteId == WasteMovement.wasteId).\
+        join(WasteType, WasteType.wasteTypeId == Waste.wasteType).\
+        join(Address, Address.addressId == WasteMovement.addressId).\
+        join(Country, Country.countryId == Address.countryId).\
+        join(Container, Container.containerId == WasteMovement.containerId).\
+        join(ContainerType, ContainerType.containerTypeId == Container.containerTypeId).\
+        join(ProducerXMovement, ProducerXMovement.wasteMovementId == WasteMovement.wasteMovementId).\
+        join(Producer, Producer.producerId == ProducerXMovement.producerId).\
+        filter(WasteMovement.quantity > quantity).\
+        order_by(WasteMovement.quantity.desc()).\
+        all()
+    return results
 
-# execute stored procedure and return results as a list of Producer objects
-def get_filtered_producers(filter):
-    results = session.execute(text(f"EXEC getFilteredProducers @filter='{filter}'"))
-    producers = []
-    for row in results:
-        producer = Producer(
-            producerId=row.producerId,
-            producerName=row.producerName,
-            grade=row.grade,
-            balance=row.balance
-        )
-        producers.append(producer)
-    return producers
 
 
 ## TEST ##
 start_time = time.time()
-producers = get_filtered_producers('john')
+producers = get_filtered_producers_orm(192)
 end_time = time.time()
 execution_time = end_time - start_time
 print(f"Execution time: {execution_time} seconds")
--- REST/readme.md
+++ REST/readme.md
@@ -5,13 +5,23 @@
     - http://localhost:5000/api/kind/saludos (GET)
     - http://localhost:5000/api/kind/saludosTo (POST) => { "quien" : "bases I" } (BODY)
 
-## Test Procedure
-CREATE PROCEDURE [dbo].[getFilteredProducers]
-   @filter VARCHAR(50)
+# Stored Procedure
+CREATE PROCEDURE GetWasteMovementsByQuantity
+    @quantity INT
 AS
 BEGIN
-   SELECT *
-   FROM producers
-   WHERE producerName LIKE '%' + @filter + '%'
+    SELECT dbo.wasteMovements.posttime, dbo.wasteMovements.quantity, dbo.containers.containerName, dbo.wastes.wasteName, dbo.wasteTypes.typeName, dbo.producers.producerName, dbo.countries.countryName
+    FROM dbo.wasteMovements 
+    INNER JOIN dbo.wastes ON dbo.wasteMovements.wasteId = dbo.wastes.wasteId 
+    INNER JOIN dbo.wasteTypes ON dbo.wastes.wasteType = dbo.wasteTypes.wasteTypeId 
+    INNER JOIN dbo.addresses ON dbo.wasteMovements.addressId = dbo.addresses.addressId 
+    INNER JOIN dbo.countries ON dbo.addresses.countryId = dbo.countries.countryId 
+    INNER JOIN dbo.containers ON dbo.wasteMovements.containerId = dbo.containers.containerId 
+    INNER JOIN dbo.containerTypes ON dbo.containers.containerTypeId = dbo.containerTypes.containerTypeId 
+    INNER JOIN dbo.producersXmovements ON dbo.wasteMovements.wasteMovementId = dbo.producersXmovements.wasteMovementId 
+    INNER JOIN dbo.producers ON dbo.producersXmovements.producerId = dbo.producers.producerId
+    WHERE quantity > @quantity ORDER BY quantity DESC;
 END
+
+- EXEC GetWasteMovementsByQuantity @quantity = 992
 
